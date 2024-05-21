869e62839ec4160416d0e62cf6c5fe59061f3a04
abcded-art <66503432+abcded-art@users.noreply.github.com>
2024-05-21 19:14:36 +0900

    Delete awswiki directory

--- awswiki/photo/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-from .models import Photo
-
-class PhotoDateFilter(filters.FilterSet):
-    date = filters.DateFilter(field_name="date", lookup_expr='date')
-
-    class Meta:
-        model = Photo
-        fields = ['date']
